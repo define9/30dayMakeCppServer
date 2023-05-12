57b7a063deff45cec8785a2a220b3babd6b85db8
define9 <define9@qq.com>
2023-05-12 11:37:02 +0800

    还原客户端

--- day11/client.cpp
+++ day11/client.cpp
@@ -1,25 +1,47 @@
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include <iostream>
 
-#include "src/Connection.h"
 #include "src/util.h"
 
-int main() {
-  Socket* socket = new Socket();
-  InetAddress* addr = new InetAddress("127.0.0.1", 8888);
-  socket->connect(addr);
-  int fd = socket->getFd();
-  Connection* conn = new Connection(socket, addr);
-  conn->setRecvConnection([=](Buffer* buf) -> bool {
-    std::cout << buf->c_str() << std::endl;
-    return false;
-  });
+#define BUFFER_SIZE 512
 
+int main() {
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in serv_addr;
+  bzero(&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serv_addr.sin_port = htons(8888);
+
+  connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));  // 直接连接服务器
   while (true) {
-    std::string str;
-    std::cin >> str;
-    conn->write(str, true);
-    conn->read(true);
+    char buf[BUFFER_SIZE];
+    bzero(&buf, sizeof(buf));
+    scanf("%s", buf);
+    ssize_t write_bytes = write(sockfd, buf, sizeof(buf));
+    printf("write size: %d\n", write_bytes);
+    if (write_bytes == -1) {
+      printf("socket already disconnected, can't write any more!\n");
+      break;
+    }
+    bzero(&buf, sizeof(buf));
+    ssize_t read_bytes = read(sockfd, buf, sizeof(buf));
+    printf("read size: %d\n", write_bytes);
+    if (read_bytes > 0) {
+      printf("message from server: %s\n", buf);
+    } else if (read_bytes == 0) {
+      printf("server socket disconnected!\n");
+      break;
+    } else if (read_bytes == -1) {
+      close(sockfd);
+      errif(true, "socket read error");
+    }
   }
-  delete conn;
+  close(sockfd);
   return 0;
 }
